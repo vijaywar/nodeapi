8cc1df716ccc6e6b1956a1d2d9678f2fddd0b80a
vijaykanth.reddy <vijaykanthbc11@gmail.com>
2021-05-30 11:07:04 +0530

    All fixed

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ function getdatal(datal,response){
       response.send(data)
     })
 }
-var server = app.listen(8080, function(){
+var server = app.listen(process.env.PORT || 8080, function(){
     var host = server.address().address
     var port = server.address().port
     console.log("REST API demo app listening at http://%s:%s", host, port)
